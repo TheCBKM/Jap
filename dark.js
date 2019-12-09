0118981579a955c9bdd6c3d27c12f4c80a3acfe0
cbkm-company <you@example.com>
2019-12-10 02:11:08 +0530

    wow

---
+++ dark.js
@@ -0,0 +1,44 @@
+japs=[],x=0,y=0,count=0
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  x=windowWidth/100
+  y=windowHeight/100
+}
+
+function draw() {
+    background(0);
+  if(count%108==0&&count!=0){
+        background(random(255),random(255),random(255));
+
+  }
+textSize(32);
+fill(0, 102, 153);
+text(count%108, 50*x, 50*y);
+  japs.map(j=>{
+    fill(j.r,j.g,j.b,j.a)
+    ellipse(j.x, j.y, 55, 55);
+    fill(0, 102, 153);
+textSize(20);
+    text(j.text, j.x-10, j.y+10);
+
+  if(j.y<98*y)
+    j.y+=5  
+  })
+  
+}
+
+
+function mouseClicked() {
+  count++
+  if(count%108==0)
+    japs=[]
+  console.log(mouseX,mouseY)
+  d={
+    x:mouseX,
+    y:mouseY,
+    text:'राम',
+    r:random(100,255),g:random(100,255),b:random(100,255),a:random(100,255)
+  }
+  japs.push(d)
+
+}
